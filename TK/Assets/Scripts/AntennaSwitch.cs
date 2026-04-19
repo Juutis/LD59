ac140f995a9e11b100abc01d33879bf1e95425db
Juutis <juutis@seri.fi>
2026-04-19 16:30:44 +0300

    World assets

---
+++ TK/Assets/Scripts/AntennaSwitch.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class AntennaSwitch : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite activated;
+    private GameObject player;
+    private SpriteRenderer rend;
+
+    [SerializeField]
+    private AntennaAnimator anim;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var selfPos = transform.position;
+        selfPos.y = 0;
+        var playerPos = player.transform.position;
+        playerPos.y = 0;
+        if (Vector3.Distance(selfPos, playerPos) < 0.5f)
+        {
+            rend.sprite = activated;
+            anim.enabled = true;
+        }
+    }
+}
