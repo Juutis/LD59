d73e8ae4acc0f123126d3d6f9af251cd9df24871
Juutis <juutis@seri.fi>
2026-04-20 17:02:15 +0300

    Build and stuff

---
+++ TK/Assets/Scripts/ScrollingText.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class ScrollingText : MonoBehaviour
+{
+    private InputAction nextLevelAction;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        nextLevelAction = InputSystem.actions.FindAction("NextLevel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var speed = 150.0f;
+        if (nextLevelAction.IsPressed())
+        {
+            speed = speed * 20;
+        }
+        transform.position += Vector3.up * speed * Time.deltaTime;
+        if (transform.position.y > 2500)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
